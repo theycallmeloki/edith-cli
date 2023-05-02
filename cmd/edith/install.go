2f91989d5960d32569747e4c8823f66380f77f3d
theycallmeloki <dude.1996@live.com>
2023-05-02 11:21:42 +0530

    Update

--- cmd/edith/install.go
+++ cmd/edith/install.go
@@ -173,106 +173,194 @@ const ansiblePlaybook = `
         executable: /bin/bash
       become: no
 
-    - name: Install Minikube
-      block:
-        - name: Install conntrack (required for Minikube)
-          apt:
-            name: conntrack
-            state: present
-          become: yes
+  pre_tasks:
+    - name: Create minikube user
+      user:
+        name: minikube_user
+        system: yes
+        create_home: yes
+        home: /home/minikube_user
+        shell: /bin/bash
 
-        - name: Download Minikube binary
-          get_url:
-            url: https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-            dest: /tmp/minikube
-            mode: '0755'
-
-        - name: Install Minikube binary to /usr/local/bin
-          command: sudo install /tmp/minikube /usr/local/bin/
-          args:
-            removes: /tmp/minikube
-          become: yes
-
-        - name: Check if kubectl command is available
-          command: kubectl version --client
-          register: kubectl_check_result
-          ignore_errors: true
-          become: 'no'
-
-        - name: Set kubectl_available variable
-          set_fact:
-            kubectl_available: '{{ kubectl_check_result.rc == 0 }}'
-
-        - name: Display kubectl_available variable
-          debug:
-            var: kubectl_available
-
-        - name: Install kubectl
-          block:
-          - name: Get the latest stable kubectl version
-            command: curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt
-            register: stable_kubectl_version
-            become: no
-
-          - name: Download kubectl binary
-            get_url:
-              url: "https://storage.googleapis.com/kubernetes-release/release/{{ stable_kubectl_version.stdout }}/bin/linux/amd64/kubectl"
-              dest: /usr/local/bin/kubectl
-              mode: '0755'
-            become: yes
-
-          - name: Display kubectl version
-            debug:
-              msg: "{{ stable_kubectl_version.stdout }}"
-        
-        - name: Download pachctl_1.12.5_amd64.deb
-          get_url:
-            url: https://github.com/pachyderm/pachyderm/releases/download/v1.12.5/pachctl_1.12.5_amd64.deb
-            dest: /tmp/pachctl_1.12.5_amd64.deb
-            mode: 0644
-
-        - name: Install pachctl
-          ansible.builtin.apt:
-            deb: /tmp/pachctl_1.12.5_amd64.deb
-
-        - name: Create minikube user
-          user:
-            name: minikube_user
-            system: yes
-            create_home: yes
-            home: /home/minikube_user
-            shell: /bin/bash
-        
-        - name: Create Minikube service file
-          copy:
-            content: |
-              [Unit]
-              Description=Minikube
-              After=network.target
-        
-              [Service]
-              User=minikube_user
-              Group=minikube_user
-              Type=oneshot
-              RemainAfterExit=yes
-              Environment="MINIKUBE_HOME=/home/minikube_user"
-              ExecStart=/usr/local/bin/minikube start --wait=all --cpus=2 --memory=2GB
-              ExecStop=/usr/local/bin/minikube stop
-        
-              [Install]
-              WantedBy=multi-user.target
-            dest: /etc/systemd/system/minikube.service
-            owner: root
-            group: root
-            mode: 0644
-
-        - name: Reload systemd daemon
-          systemd:
-            daemon_reload: yes
-
-        - name: Enable and start Minikube service
-          systemd:
-            name: minikube.service
-            enabled: yes
-            state: started
+  roles:
+    - role: gantsign.minikube
+      become: no
+      become_user: minikube_user
 `
+
+// const ansiblePlaybook = `
+// - name: Setup Node
+//   hosts: all
+//   become: yes
+//   tasks:
+//     - name: Update packages
+//       apt:
+//         update_cache: yes
+//         cache_valid_time: 3600
+
+//     - name: Install required packages
+//       apt:
+//         name:
+//           - apt-transport-https
+//           - ca-certificates
+//           - curl
+//           - gnupg
+//           - lsb-release
+//           - build-essential
+//           - jq
+//           - htop
+//         state: present
+
+//     - name: Add Docker repository key
+//       ansible.builtin.apt_key:
+//         url: https://download.docker.com/linux/ubuntu/gpg
+//         state: present
+
+//     - name: Add Docker repository
+//       ansible.builtin.apt_repository:
+//         repo: "deb [arch=amd64] https://download.docker.com/linux/ubuntu {{ ansible_distribution_release }} stable"
+//         state: present
+
+//     - name: Install Docker
+//       apt:
+//         name: docker-ce
+//         state: present
+//         update_cache: yes
+
+//     - name: Add user to docker group
+//       user:
+//         name: "{{ ansible_user }}"
+//         groups: docker
+//         append: yes
+
+//     - name: Check if NVM is installed
+//       stat:
+//         path: "{{ ansible_env.HOME }}/.nvm/nvm.sh"
+//       register: nvm_installed 
+
+//     - name: Display nvm_installed
+//       debug:
+//         msg: "{{ nvm_installed }}"
+
+//     - name: Install NVM
+//       shell: |
+//         curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+//       args:
+//         executable: /bin/bash
+//       become: no
+//       when: not nvm_installed.stat.exists
+
+//     - name: Install Node.js 16 and update npm 
+//       shell: |
+//         export NVM_DIR="$HOME/.nvm"
+//         [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+//         nvm install 16.19.1
+//         nvm use v16.19.1
+//         npm i -g npm
+//       args:
+//         executable: /bin/bash
+//       become: no
+
+//     - name: Install Minikube
+//       block:
+//         - name: Install conntrack (required for Minikube)
+//           apt:
+//             name: conntrack
+//             state: present
+//           become: yes
+
+//         - name: Download Minikube binary
+//           get_url:
+//             url: https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+//             dest: /tmp/minikube
+//             mode: '0755'
+
+//         - name: Install Minikube binary to /usr/local/bin
+//           command: sudo install /tmp/minikube /usr/local/bin/
+//           args:
+//             removes: /tmp/minikube
+//           become: yes
+
+//         - name: Check if kubectl command is available
+//           command: kubectl version --client
+//           register: kubectl_check_result
+//           ignore_errors: true
+//           become: 'no'
+
+//         - name: Set kubectl_available variable
+//           set_fact:
+//             kubectl_available: '{{ kubectl_check_result.rc == 0 }}'
+
+//         - name: Display kubectl_available variable
+//           debug:
+//             var: kubectl_available
+
+//         - name: Install kubectl
+//           block:
+//           - name: Get the latest stable kubectl version
+//             command: curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt
+//             register: stable_kubectl_version
+//             become: no
+
+//           - name: Download kubectl binary
+//             get_url:
+//               url: "https://storage.googleapis.com/kubernetes-release/release/{{ stable_kubectl_version.stdout }}/bin/linux/amd64/kubectl"
+//               dest: /usr/local/bin/kubectl
+//               mode: '0755'
+//             become: yes
+
+//           - name: Display kubectl version
+//             debug:
+//               msg: "{{ stable_kubectl_version.stdout }}"
+        
+//         - name: Download pachctl_1.12.5_amd64.deb
+//           get_url:
+//             url: https://github.com/pachyderm/pachyderm/releases/download/v1.12.5/pachctl_1.12.5_amd64.deb
+//             dest: /tmp/pachctl_1.12.5_amd64.deb
+//             mode: 0644
+
+//         - name: Install pachctl
+//           ansible.builtin.apt:
+//             deb: /tmp/pachctl_1.12.5_amd64.deb
+
+//         - name: Create minikube user
+//           user:
+//             name: minikube_user
+//             system: yes
+//             create_home: yes
+//             home: /home/minikube_user
+//             shell: /bin/bash
+        
+//         - name: Create Minikube service file
+//           copy:
+//             content: |
+//               [Unit]
+//               Description=Minikube
+//               After=network.target
+        
+//               [Service]
+//               User=minikube_user
+//               Group=minikube_user
+//               Type=oneshot
+//               RemainAfterExit=yes
+//               Environment="MINIKUBE_HOME=/home/minikube_user"
+//               ExecStart=/usr/local/bin/minikube start --wait=all --cpus=2 --memory=2GB
+//               ExecStop=/usr/local/bin/minikube stop
+        
+//               [Install]
+//               WantedBy=multi-user.target
+//             dest: /etc/systemd/system/minikube.service
+//             owner: root
+//             group: root
+//             mode: 0644
+
+//         - name: Reload systemd daemon
+//           systemd:
+//             daemon_reload: yes
+
+//         - name: Enable and start Minikube service
+//           systemd:
+//             name: minikube.service
+//             enabled: yes
+//             state: started
+// `
