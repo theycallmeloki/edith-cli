273e5f37405fa1e385692619145ad46ae595e7ba
theycallmeloki <dude.1996@live.com>
2023-05-04 21:19:13 +0530

    Update

--- cmd/edith/install.go
+++ cmd/edith/install.go
@@ -175,6 +175,15 @@ const ansiblePlaybook = `
       args:
         executable: /bin/bash
       become: no
+    
+    - name: Run pm2 startup
+      shell: |
+        export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+        pm2 startup
+      args:
+        executable: /bin/bash
+      become: yes
 
     - name: Install Minikube
       block:
